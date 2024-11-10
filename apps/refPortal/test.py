f1b3499403293447839300d4b020d0af4d4d3f0a
guyshachar <guyshachar.acc@gmail.com>
2024-11-10 13:39:47 +0200

    update

--- apps/refPortal/test.py
+++ apps/refPortal/test.py
@@ -1,17 +1,19 @@
+import logging
 import asyncio
 from playwright.async_api import async_playwright
 
 async def run():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
     async with async_playwright() as p:
-        print("Hello1")
+        logger.info("Hello1")
         browser = await p.chromium.launch(headless=True)
-        print("Hello2")
+        logger.info("Hello2")
         page = await browser.new_page()
-        print("Hello3")
+        logger.info("Hello3")
         await page.goto('https://example.com')
         await page.screenshot(path='example.png')
         await browser.close()
 
 if __name__ == "__main__":
-    print("Hello, World!")
     asyncio.run(run())
