413d17423172430fd25e193c55c48193ab572b0d
guyshachar <guyshachar.acc@gmail.com>
2024-11-20 01:51:19 +0200

    run referees in parallel tasks, send whatsapp messages

--- apps/refPortal/refPortalAppLocal.py
+++ apps/refPortal/refPortalAppLocal.py
@@ -8,6 +8,12 @@ import json
 import os
 import asyncio
 from playwright.async_api import async_playwright
+from twilio.rest import Client
+#from html2image import Html2Image
+from bs4 import BeautifulSoup
+import html2text
+import boto3
+from botocore.exceptions import ClientError
 
 #from playwright.sync_api import sync_playwright
 #from plyer import notification
@@ -23,22 +29,45 @@ class RefPortalApp():
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.logger = logging.getLogger(__name__)
      
-        self.logger.info("Ref Portal from AppDaemon")
+        self.logger.info("Ref Portal")
         
-        """   
-        self.url = self.args['url']
-        self.pollingInterval = self.args['polling_interval']
-        self.topic = self.args['topic']
-        """   
-        self.url = os.environ.get('loadUrl')
-        self.pollingInterval = int(os.environ.get('loadInterval'))
+        self.url = os.environ.get('loadUrl') or 'https://ref.football.org.il/login'
+        self.pollingInterval = int(os.environ.get('loadInterval') or '5000')
         self.topic = 'my/mqtt/refPortal/new'
 
-        self.refId = os.environ.get('refPortalId')
-        self.refPass = os.environ.get('refPortalPassword')
-        self.id = os.environ.get('refId')
+        secretName = os.environ.get('refPortalSM') or "refPortal/referees"
+        refPortalSecret = self.get_secret(secretName)
+        refereesKey = refPortalSecret and refPortalSecret.get("refPortal_referees", None)
 
-        self.logger.info(f'url={self.url}')
+        if refereesKey:
+            #self.logger.info(f'refereesSM: {refereesKey}')
+            self.referees = json.loads(refereesKey)
+        else:
+            self.referees = [
+            {
+                "name": "guy",
+                "refId": "43679",
+                "refPass": "S073XdLR",
+                "id": "025092271",
+                "mobile": "+972547799979"
+            },
+            {
+                "name": "erez",
+                "refId": "26054",
+                "refPass": "26054",
+                "id": "031869779",
+                "mobile": "+972547799979"
+            }
+            ]
+        self.logger.info(f'Referees#: {len(self.referees)}')
+        
+        account_sid = 'ACc28a1c0a3ee4b2d606c0250819f8ae85'
+        auth_token = refPortalSecret and refPortalSecret.get("twilio_auth_token", None) or 'c0aeae45f047b713b0b4d72b5d85ffb2'
+        self.twilioClient = Client(account_sid, auth_token)
+        self.twilioFromMobile = '+14155238886'
+        self.twilioSend = True
+
+        self.logger.info(f'url={self.url} interval={self.pollingInterval}')
 
         pass
 
@@ -46,68 +75,49 @@ class RefPortalApp():
         self.start()
         pass
 
-    async def login(self):
-        async with async_playwright() as p:
-            self.logger.info(f'login')
-            result = ''
+    def get_secret(self, secretName):
+        self.logger.info(f'secret: {secretName}')
+        region_name = "il-central-1"
+        secret = None
 
-            try:
-                #browser1 = await p.chromium.launch(headless=True)  # Launch browser (headless=True for no UI)
-                browser = await p.chromium.launch(headless=True, args=['--no-sandbox','--disable-setuid-sandbox','--disable-gpu','--single-process'])
-                self.logger.info(f'launch')
-                page = await browser.new_page()
-                self.logger.info(f'new page')
-                # Navigate to the URL
-                await page.goto(self.url)  # Replace with your target URL
-                self.logger.info(f'goto {self.url}')
+        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name=region_name
+        )
 
-                title = await page.title()
-                self.logger.info(f'{title}')
-                # Perform actions, e.g., print the title of the page
-                # print("Page title is:", title)
-                
-                input_elements = await page.query_selector_all('input')
-                
-                usernameField = input_elements[0]
-                await usernameField.fill(self.refId)
+        try:
+            self.logger.info(f'secret: Get Value')
+            get_secret_value_response = client.get_secret_value(
+                SecretId=secretName
+            )
+            self.logger.info(f'secret: Get Value String')
+            secretStr = get_secret_value_response['SecretString']
+            #self.logger.info(f'secretStr: {secretStr}')
+            secret = json.loads(secretStr)
+            #self.logger.info(f'secretStr: {secret}')
+            return secret
+ 
+        except ClientError as e:
+            # For a list of exceptions thrown, see
+            # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            #raise e
+            self.logger.error(f'secret clientError: {e}')
+            pass
+        except Exception as e:
+            self.logger.error(f'secret: {e}')
+            pass
 
-                passwordField = input_elements[1]
-                await passwordField.fill(self.refPass)
-
-                idField = input_elements[2]
-                await idField.fill(self.id)
-
-                # Find the submit button and click it
-                button_elements = await page.query_selector_all('button')
-                mainButton = button_elements[0]
-                await mainButton.click()  # Replace selector as needed
-
-                await page.wait_for_timeout(3000)  # Wait for 2 seconds
-
-                # Execute the callback function
-                result = await self.readPortal(page)
-
-                pass
-
-            except Exception as ex:
-                self.logger.info(f'Error: {ex}')
-                self.logger.error(f'Error: {ex}')
-                pass
-    
-            finally:
-                self.logger.info(f'close')
-                await browser.close()
-                return result
-                pass
+        return None
 
     async def readPortal(self, page):
         result = ''
         try:
             self.logger.info(f'before readPortal')
             table_elements = await page.query_selector_all('table')
-            table_html = await table_elements[0].inner_html()
-            table_text = await table_elements[0].inner_text()
-            result = table_text
+            if len(table_elements) > 0:
+                result = table_elements[0]
             pass
 
         except Exception as ex:
@@ -119,48 +129,181 @@ class RefPortalApp():
             #print(page.content())
             return result
 
-    def send_notification(self, title, message):
+    async def convert_table_to_text(self, html):
+        # Parse the HTML using BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+
+        h = html2text.HTML2Text()
+        # Ignore converting links from HTML
+        h.ignore_links = True
+
+        # Extract table rows
+        rows = soup.find_all('tr')
+
+        # Get headers (assumes the first row contains headers)
+        headers = [th.get_text(strip=True) for th in rows[0].find_all('th')]
+
+        # Process each subsequent row and map to headers
+        result = []
+        if len(rows)==1:
+            result.append(f"אין שיבוצים")
+        else:
+            for row in rows[1:]:  # Skip the header row
+                cells = row.find_all('td')
+                if len(cells) == len(headers):  # Regular rows
+                    for header, cell in zip(headers, cells):
+                        cellText = cell.get_text(strip=True)
+                        if header or cellText:
+                            result.append(f"{header}: {cellText}")
+                elif len(cells) == 1:  # Row with colspan
+                    result.append(h.handle(cells[0].decode_contents()))
+
+        return "\n".join(result)
+
+    async def send_notification(self, title, message, fromMobile, toMobile, toName):
         #audio.play('')
+        #templateSid = 'HX6c220c925c90ee34328a453facd64083'
+        #templateSid2 = 'HX229f5a04fd0510ce1b071852155d3e75'
+        #title = '12/1'
+        #message = '3pm'
+        
+        #message = 'שובצת ביום רביעי למשחק רבע גמר גביע האלופות בשעה 22:00 למשחק ברצלונה נגד מנצ׳סטר סיטי'
+        #message = 'סוג דוח    תאריך   מסגרת משחקים    קבוצה ביתית: קבוצה אורחת        מח.     מגרש    ניווט   סטטוס   ?'
+        #fromMobile = fromMobile or '+14155238886'
+        #toMobile = toMobile or '+972547799979'
+        #self.logger.info(message)
+        #message1 = message#.replace('״', "")
+        #hti = Html2Image()
+        #imageFile = f"output_image_{uuid.uuid4()}.png"
+        #hti.screenshot(html_str=message, save_as=imageFile)
 
-        notification.notify(
-            title=title,
-            message=message,
-            timeout=15,  # Time in seconds to display the notification
-            ticker=title
-        )
+        message1 = f' שיבוצים של {toName}:' + '\n' + message
+        message1 = message1.replace('"','\"')
+        #message1 = message1[:10]
+        #content_variables = '{"1":"'+message1+'"}'
+        #self.logger.info(f'SENT: {content_variables}')
 
-    def notifyUpdate(self, result):
-        self.logger.info(f'result: {result}')
-        js = json.dumps(result)
+        if self.twilioSend:
+            """
+            sentMessage = self.twilioClient.messages.create(
+                from_=f'whatsapp:{fromMobile}',
+                content_sid=templateSid2,
+                content_variables={"1":"409173"},
+                to=f'whatsapp:{toMobile}'
+            )
+            """
+            sentMessage = self.twilioClient.messages.create(
+                from_=f'whatsapp:{fromMobile}',
+                to=f'whatsapp:{toMobile}',
+                body=f'{message1}'
+            )
+            self.logger.info(f'message.id: {sentMessage.sid} {sentMessage.status}')
+
+    async def notifyUpdate(self, referee):
+        self.logger.info(f'result: {referee["name"]}: {referee["last_hText"]}')
         
         try:
             # Example usage
-            #self.send_notification("עדכון שיבוץ בפורטל", result)
+            await self.send_notification("עדכון שיבוץ בפורטל", referee["last_hText"], self.twilioFromMobile, referee["mobile"], referee["name"])
             #self.call_service("mqtt/publish", topic=self.topic, payload=json.dumps(result), qos=1)
             pass
         finally:
             pass
     
+    async def checkReferee(self, referee):
+        page = referee["page"]
+        await page.reload()
+        await page.wait_for_load_state("load")
+        self.logger.info(f'url={page.url}')
+        input_elements = await page.query_selector_all('input')
+        if page.url == 'about:blank' or len(input_elements) >= 3:
+            await self.login(referee)
+        
+        title = await page.title()
+        self.logger.info(f'{referee["name"]}: {title}')
+        
+        # Execute the callback function
+        result = await self.readPortal(page)
+        table_outer_html = await result.evaluate("element => element.outerHTML")
+        #self.logger.info(f'table_outer_html: {table_outer_html}')
+        hText = await self.convert_table_to_text(table_outer_html)
+        #self.logger.info(f'hText: {hText}')
+
+        if hText:
+            if hText != referee["last_hText"]:
+                referee["last_hText"] = hText
+                await self.notifyUpdate(referee)
+            else:
+                self.logger.info(f'{referee["name"]}: No update...')
+                pass
+        else:
+            pass
+
+    async def login(self, referee):
+        # Navigate to the URL
+        self.logger.info(f'login')
+        page = referee['page']
+        await page.goto(self.url) 
+        await page.reload()
+        await page.wait_for_load_state("load")
+        #title = await page.title()
+        #self.logger.info(f'{referee["name"]}: {title}')
+        
+        input_elements = await page.query_selector_all('input')
+        
+        usernameField = input_elements[0]
+        await usernameField.fill(referee["refId"])
+
+        passwordField = input_elements[1]
+        await passwordField.fill(referee["refPass"])
+
+        idField = input_elements[2]
+        await idField.fill(referee["id"])
+
+        # Find the submit button and click it
+        button_elements = await page.query_selector_all('button')
+        mainButton = button_elements[0]
+        await mainButton.click()  # Replace selector as needed
+
+        await page.wait_for_timeout(3000)  # Wait for 2 seconds
+
     async def start(self):
         self.logger.info('Start')
-        lastResult = ''
-        while True:
-            try:
-                result = await self.login()  # Call the method
-                if result:
-                    if result != lastResult:
-                        lastResult = result
-                        self.notifyUpdate(result)
-                    else:
-                        self.logger.info(f'No update...')
-                        pass
-                else:
-                    pass
-            except Exception as ex:
-                self.logger.error(f'start error: {ex}')
+        browser = None
+        
+        try:
+            async with async_playwright() as p:
+                browser = await p.chromium.launch(headless=True, args=['--no-sandbox','--disable-setuid-sandbox','--disable-gpu'])
+                self.logger.info(f'launch')
 
-            finally:
-                await asyncio.sleep(self.pollingInterval / 1000)
+                for referee in self.referees:
+                    self.logger.info(f'{json.dumps(referee)}')
+                    referee["page"] = await browser.new_page()
+                    referee["last_hText"] = ''
+                    self.logger.info(f'{referee["name"]}: new page')
+                    pass
+
+                while True:
+                    try:
+                        refereesTasks = []
+                        for referee in self.referees:
+                            refereesTasks.append(asyncio.create_task(self.checkReferee(referee)))
+                        tasksResults = await asyncio.gather(*refereesTasks)
+                    except Exception as ex:
+                        self.logger.error(f'Error: {ex}')
+                        pass
+                    finally:
+                        await asyncio.sleep(self.pollingInterval / 1000)
+                        pass
+
+        except Exception as ex:
+            self.logger.error(f'Error: {ex}')
+            pass
+        
+        finally:
+            self.logger.info(f'close')
+            await browser.close()
+            pass
 
 if __name__ == "__main__":
     app = RefPortalApp()
